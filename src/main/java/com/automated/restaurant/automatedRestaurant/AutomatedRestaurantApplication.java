d907e855b552d4ddf9ba63ec774627c133f86fc8
erico cavalcanti <ecocaval@hotmail.com>
2024-02-22 23:22:17 -0300

    firts template

---
+++ src/main/java/com/automated/restaurant/automatedRestaurant/AutomatedRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.automated.restaurant.automatedRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedRestaurantApplication.class, args);
+	}
+
+}
